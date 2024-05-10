2b66303fd068b941fd9bd9179a713249c8648173
cagrical <93766289+cagrical@users.noreply.github.com>
2024-05-10 15:59:53 +0300

    Add files via upload

---
+++ Activity10/Activity10_Slider/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  captions:true,
+			  randomStart: true,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  pause:3000,
+			  pagerType:'short'
+			});
+    	});
